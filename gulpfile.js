c4a12248ee05a87e96af01b53c0049216ac82ec8
vincentn80 <vincent.nel@alligo.com>
2025-01-21 11:45:45 +0100

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+
+// Paths for SCSS files
+const paths = {
+  // scss: 'src/**/*.scss', // Adjust path to your SCSS files
+  scss: 'src/components/**/*.scss', // Adjust path to your SCSS files
+  dist: 'dist/scss', // Output folder for SCSS
+};
+
+// Task to copy SCSS files to dist
+gulp.task('copy-scss', () => {
+  return gulp.src(paths.scss)
+    .pipe(gulp.dest(paths.dist));
+});
+
+// Task to compile SCSS to CSS (optional)
+gulp.task('compile-scss', () => {
+  return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(`${paths.dist}/css`));
+});
+
+// Default task
+gulp.task('default', gulp.series('copy-scss', 'compile-scss'));
